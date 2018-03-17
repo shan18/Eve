7aa1ac764469985b5519545960b101a08269bea0
Shantanu Acharya <thegeek.004@gmail.com>
2018-03-17 22:34:37 +0530

    preprocessing: mapping each line to its id

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,17 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 17 21:24:20 2018
+# Importing the necessary packages
+import numpy as np
+import tensorflow as tf
+import re
+import time
 
-@author: shan
-"""
 
+# Importing the dataset
+lines = open('dataset/movie_lines.txt', encoding='utf-8', errors='ignore').read().split('\n')
+conversations = open('dataset/movie_conversations.txt', encoding='utf-8', errors='ignore').read().split('\n')
+
+# Mapping each line to its id in a dictionary
+id2line = {}
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[-1]
