708425fa6c385be2f1ff557311cf2bb5b760ea16
Shantanu Acharya <thegeek.004@gmail.com>
2018-03-17 23:39:33 +0530

    preprocessing: separating questions and answers from the dataset

--- chatbot.py
+++ chatbot.py
@@ -15,3 +15,16 @@ for line in lines:
     _line = line.split(' +++$+++ ')
     if len(_line) == 5:
         id2line[_line[0]] = _line[-1]
+
+# Create a list of all conversations
+conversations_ids = []
+for conversation in conversations[:-1]:  # last row is empty
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'", "").split(', ')
+    conversations_ids.append(_conversation)
+
+# separating questions and answers from the dataset
+questions, answers = [], []
+for conversation in conversations_ids:
+    for i in range(len(conversation) - 1):
+        questions.append(id2line[conversation[i]])
+        answers.append(id2line[conversation[i+1]])
