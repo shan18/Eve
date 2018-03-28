c1d15c81ea9d69474e82a0779f07ca8746d93d8d
Shantanu Acharya <thegeek.004@gmail.com>
2018-03-29 00:13:48 +0530

    training: get model inputs and other parameters required for training

--- training.py
+++ training.py
@@ -1,3 +1,8 @@
+import tensorflow as tf
+
+from seq2seq_architecture import model_inputs
+
+
 # Setting the hyperparameters
 epochs = 100
 batch_size = 64
@@ -9,3 +14,17 @@ learning_rate = 0.01
 learning_rate_decay = 0.9  # decay of 90%
 min_learning_rate = 0.0001  # learning_rate won't go below this
 keep_probability = 0.5
+
+
+# Define a tensorflow session
+tf.reset_default_graph()  # reset all the graphs before starting a new session
+session = tf.InteractiveSession()
+
+# Load model inputs
+inputs, targets, lr, keep_prob = model_inputs()
+
+# Set the sequence length
+sequence_length = tf.placeholder_with_default(25, None, name='sequence_length')
+
+# Get the shape of the input tensor
+input_shape = tf.shape(inputs)
